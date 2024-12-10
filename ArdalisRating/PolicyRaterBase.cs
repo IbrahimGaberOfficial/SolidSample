fc0bc19c956e935457ed8065d9ca68f7406503db
IbrahimGaberOfficial <122841058+IbrahimGaberOfficial@users.noreply.github.com>
2024-12-10 20:09:32 +0200

    Start applying Open-Closed Principle - delegate the rating business to a seprated detecated class . - as every class type is responsible for a different policy 's rating type.

---
+++ ArdalisRating/PolicyRaterBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArdalisRating
+{
+    public class PolicyRaterBase
+    {
+        protected readonly RatingEngine _engine;
+        protected readonly ConsoleLogger _logger;
+
+        public PolicyRaterBase(RatingEngine engine, ConsoleLogger logger)
+        {
+            this._engine = engine;
+            this._logger = logger;
+        }
+    }
+}
