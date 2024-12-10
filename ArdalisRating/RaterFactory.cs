4454bdc3b439007639510c2be5ec6b1f536e1a87
IbrahimGaberOfficial <122841058+IbrahimGaberOfficial@users.noreply.github.com>
2024-12-10 20:31:35 +0200

    use reflection to create the `Rater` type.

--- ArdalisRating/RaterFactory.cs
+++ ArdalisRating/RaterFactory.cs
@@ -10,23 +10,34 @@ namespace ArdalisRating
     {
         public Rater Create(Policy policy, RatingEngine engine)
         {
-            switch (policy.Type)
+            try
             {
-                case PolicyType.Auto:
-                    return new AutoPolicyRater(engine, engine.Logger);
-
-                case PolicyType.Land:
-                    return new LandPolicyRater(engine, engine.Logger);
-
-                case PolicyType.Life:
-                    return new LifePolicyRater(engine, engine.Logger);
-
-                default:
-                    //TODO: Use NULL object pattern.
-                    engine.Logger.Log("Unknown policy type");
-                    return null;
-
+                var rater = (Rater)Activator.CreateInstance(Type.GetType($"ArdalisRating.{policy.Type}PolicyRater"), new object[] { engine, engine.Logger });
+                return rater;
             }
+            catch { 
+                return null;
+            }
+
+            #region using switch
+            //switch (policy.Type)
+            //{
+            //    case PolicyType.Auto:
+            //        return new AutoPolicyRater(engine, engine.Logger);
+
+            //    case PolicyType.Land:
+            //        return new LandPolicyRater(engine, engine.Logger);
+
+            //    case PolicyType.Life:
+            //        return new LifePolicyRater(engine, engine.Logger);
+
+            //    default:
+            //        //TODO: Use NULL object pattern.
+            //        engine.Logger.Log("Unknown policy type");
+            //        return null;
+
+            //} 
+            #endregion
         }
     }
 }
